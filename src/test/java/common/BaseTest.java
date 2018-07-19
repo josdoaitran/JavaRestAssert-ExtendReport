9605e094414ed0582b682e46b11841d71b66ce66
josdoaitran <josdoaitran@gmail.com>
2018-07-19 07:39:19 +0700

    update remote allure listener

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -1,18 +1,14 @@
 package common;
 
 import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.filter.Filter;
 import com.jayway.restassured.specification.RequestSpecification;
-import common.listeners.AllureListener;
-import common.reports.AllureRestAssured;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Listeners;
 
 import java.util.logging.Logger;
 
 import static common.Utilities.sDirPath;
-import common.reports.AllureRestAssured;
+
 /**
  * Created by DoaiTran
  */
